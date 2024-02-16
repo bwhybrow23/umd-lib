965c1bd877d4d34946cbc16a936656481d32ea7e
Vinicius Egidio <me@vinicius.io>
2024-02-16 19:04:38 +0000

    🔖 Bumped version to 24.2.16

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .binaryTarget(
             name: "UMD",
             url: "https://github.com/vegidio/umd-lib/releases/download/24.2.16/umd-xcframework.zip",
-            checksum: "7a55554e5c7819ee03136c5eb23935d80b5b3d47273def8514e579f6ac2b37de"
+            checksum: "4ff6e931ae736b11338c7a1d606cd653348f0cb68776f74523afd8e694112b82"
         )
     ]
 )
