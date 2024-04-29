ceb5a5b4e0f190c4812c6b4c90b20c387a426436
Vinicius Egidio <me@vinicius.io>
2024-04-29 18:25:38 +0000

    ⬆️ Upgraded Klopik to version 24.4.7

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "UMD",
-            url: "https://github.com/vegidio/umd-lib/releases/download/24.4.4/umd-xcframework.zip",
-            checksum: "7bc4031ffe0ac0f3ea20ce6ff428ee0edda8e32226ffa21a7570336769a41928"
+            url: "https://github.com/vegidio/umd-lib/releases/download/24.4.5/umd-xcframework.zip",
+            checksum: "3a17f4ac82f42496b2d2ae51422ce52e0c04876df152dfb2e82eb5501abb1f15"
         )
     ]
 )
